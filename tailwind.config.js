7b477b1c637371b453fc874b1e06c40fa0f26fa5
Success Bright <bs3webdevelop@gmail.com>
2024-04-14 15:25:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+    },
+  },
+  variants: {},
+  plugins: [],
+};
